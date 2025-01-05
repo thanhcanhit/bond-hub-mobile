10801462a55ab57b1104f45af6a6c56b8ce5fcdd
kientrank3 <bankienthanthien@gmail.com>
2025-01-05 20:32:48 +0700

    Add prettier & husky

--- components/ui/gluestack-ui-provider/index.tsx
+++ components/ui/gluestack-ui-provider/index.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { config } from './config';
-import { ColorSchemeName, useColorScheme, View, ViewProps } from 'react-native';
-import { OverlayProvider } from '@gluestack-ui/overlay';
-import { ToastProvider } from '@gluestack-ui/toast';
-import { colorScheme as colorSchemeNW } from 'nativewind';
+import React from "react";
+import { config } from "./config";
+import { ColorSchemeName, useColorScheme, View, ViewProps } from "react-native";
+import { OverlayProvider } from "@gluestack-ui/overlay";
+import { ToastProvider } from "@gluestack-ui/toast";
+import { colorScheme as colorSchemeNW } from "nativewind";
 
-type ModeType = 'light' | 'dark' | 'system';
+type ModeType = "light" | "dark" | "system";
 
 const getColorSchemeName = (
   colorScheme: ColorSchemeName,
-  mode: ModeType
-): 'light' | 'dark' => {
-  if (mode === 'system') {
-    return colorScheme ?? 'light';
+  mode: ModeType,
+): "light" | "dark" => {
+  if (mode === "system") {
+    return colorScheme ?? "light";
   }
   return mode;
 };
 
 export function GluestackUIProvider({
-  mode = 'light',
+  mode = "light",
   ...props
 }: {
-  mode?: 'light' | 'dark' | 'system';
+  mode?: "light" | "dark" | "system";
   children?: React.ReactNode;
-  style?: ViewProps['style'];
+  style?: ViewProps["style"];
 }) {
   const colorScheme = useColorScheme();
 
@@ -36,7 +36,7 @@ export function GluestackUIProvider({
       style={[
         config[colorSchemeName],
         // eslint-disable-next-line react-native/no-inline-styles
-        { flex: 1, height: '100%', width: '100%' },
+        { flex: 1, height: "100%", width: "100%" },
         props.style,
       ]}
     >
