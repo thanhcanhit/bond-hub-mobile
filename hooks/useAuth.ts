e9cbf3bd6361a71e2119e2b51283f11ca3ef339f
kientrank3 <bankienthanthien@gmail.com>
2025-04-08 23:13:27 +0700

    Update logic check Auth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,12 +1,41 @@
 import { useEffect, useState } from "react";
 import * as SecureStore from "expo-secure-store";
+import axiosInstance from "@/lib/axios";
+import { useAuthStore } from "@/store/authStore";
 
 export const useAuth = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const { logout } = useAuthStore();
   useEffect(() => {
     const checkAuth = async () => {
-      const accessToken = await SecureStore.getItemAsync("accessToken");
-      setIsAuthenticated(!!accessToken);
+      try {
+        const accessToken = await SecureStore.getItemAsync("accessToken");
+        if (!accessToken) {
+          setIsAuthenticated(false);
+          return;
+        }
+        const refreshToken = await SecureStore.getItemAsync("refreshToken");
+        const deviceId = await SecureStore.getItemAsync("deviceId");
+        try {
+          await axiosInstance.post(
+            `${process.env.EXPO_PUBLIC_API_URL}/auth/refresh`,
+            {
+              refreshToken,
+              deviceId,
+            },
+          );
+          setIsAuthenticated(true);
+        } catch (error: any) {
+          if (error?.response?.status === 401) {
+            // Token không hợp lệ, tự động logout
+            await logout();
+          }
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error("Auth check failed:", error);
+        setIsAuthenticated(false);
+      }
     };
 
     checkAuth();
